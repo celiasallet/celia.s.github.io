8574c4933bb972751709a7540bbbc56b71b24aea
celiasallet <celiasallet@yahoo.fr>
2024-09-15 17:08:54 +0200

    responsive

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const container = document.getElementById("article-container");
 
-  // Liste des fichiers YAML à charger (un fichier pour chaque article)
-  const articles = ['article2.yml', 'article3.yml', 'article4.yml']; // Corrige la répétition
+  const articles = ['article2.yml']; //  , 'article3.yml', 'article4.yml' Corrige la répétition
 
-  // Fonction pour charger un fichier YAML
+ 
   function loadYAMLFile(yamlFile) {
     // Charger le fichier YAML
     fetch(`./assets/${yamlFile}`)
@@ -15,17 +14,15 @@ document.addEventListener("DOMContentLoaded", function () {
         return response.text();
       })
       .then((yamlText) => {
-        // Parser le YAML en objet JavaScript
+    
         const data = jsyaml.load(yamlText);
         
-        // Vérification que la clé 'images' existe et contient au moins une image
         const firstImage = data.images && data.images.length > 0 ? data.images[0] : ''; 
 
         if (!firstImage) {
           throw new Error("Aucune image trouvée pour l'article : " + yamlFile);
         }
 
-        // Utiliser les données du fichier YAML pour créer l'HTML
         const articleHTML = `
           <article class="article">
             <img class="home" src="./assets/img/${firstImage}" alt="${data.title}">
@@ -33,7 +30,6 @@ document.addEventListener("DOMContentLoaded", function () {
           </article>
         `;
 
-        // Ajouter l'article à la page
         container.innerHTML += articleHTML;
       })
       .catch((error) => {
@@ -42,7 +38,6 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   }
 
-  // Charger tous les articles
   articles.forEach((article) => loadYAMLFile(article));
 });
 
